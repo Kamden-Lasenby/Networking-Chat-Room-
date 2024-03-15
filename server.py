ca713b33758e17e34a3fef6b2014c5e900b9bacf
Kamden Riley <kervfh@umsystem.edu>
2024-03-15 01:05:54 -0500

    accept and send

--- server.py
+++ server.py
@@ -37,5 +37,28 @@ def bind_socket():
 def socket_accept():
     connection , address = s.accept()
     print("Connection has been established: " + " IP " + address[0] + " Port " + str(address[1]))
-    
+    send_commands(connection)
     connection.close()
+
+# Send commands to client
+def send_commands(conn):
+    while True:
+        cmd = input()
+
+        if cmd == "quit":
+            conn.close()
+            s.close()
+            sys.exit()
+
+        if len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024), "utf-8")
+            print(client_response, end="")
+
+
+def main():
+    create_socket()
+    bind_socket()
+    socket_accept()
+
+main()
